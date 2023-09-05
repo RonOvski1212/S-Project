bfeb47512e6ade4a4e2e73f29a099040ed1b6650
RonOvski1212 <245785gio@gmail.com>
2023-09-05 18:39:23 +0400

    test

--- my-app/src/components/app-filter/app-filter.js
+++ my-app/src/components/app-filter/app-filter.js
@@ -2,14 +2,15 @@ import "./app-filter.css";
 
 const AppFilter = (props) => {
   const buttonsData = [
-    { name: "all", label: "თანამშრომლები" },
-    { name: "rise", label: "დაწინაურება" },
-    { name: "allmoreThen1000", label: "ხელფასი 1000$ ზემოთ" },
+    { name: "all", label: "თანამშრომლები", colored: false },
+    { name: "rise", label: "დაწინაურება", colored: false },
+    { name: "allmoreThen1000", label: "ხელფასი 1000$ ზემოთ", colored: true },
   ];
 
-  const buttons = buttonsData.map(({ name, label }) => {
+  const buttons = buttonsData.map(({ name, label, colored }) => {
     const active = props.filter === name;
     const clazz = active ? "btn-light" : "btn-outline-light";
+    const style = colored ? { color: "red" } : null;
 
     return (
       <button
@@ -17,6 +18,7 @@ const AppFilter = (props) => {
         type="button"
         key={name}
         onClick={() => props.onFilterSelect(name)}
+        style={style}
       >
         {label}
       </button>
